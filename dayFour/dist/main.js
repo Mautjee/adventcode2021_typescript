821b2673adec21961527beaa7e9c5406f3f671ad
Mauro Eijsenring <mauro@eijsenring.com>
2022-01-05 15:55:26 +0100

    Deleted dist folder
    Dist folder is not can be compiled using npm start command

--- dayFour/dist/main.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const partOne_1 = require("./partOne");
-const partTwo_1 = require("./partTwo");
-const getData_1 = require("./getData");
-console.log("answer part 1 = " + (0, partOne_1.partOne)((0, getData_1.getData)("data.txt")));
-console.log("answer part 2 = " + (0, partTwo_1.partTwo)((0, getData_1.getData)("data.txt")));
-//# sourceMappingURL=main.js.map

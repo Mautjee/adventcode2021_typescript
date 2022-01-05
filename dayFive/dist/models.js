f89698ccb770f20edbc6eae666b1dc4dd9cd5dbb
Mauro Eijsenring <mauro@eijsenring.com>
2022-01-05 15:56:34 +0100

    Deleted dist folder
    When running 'npm start' the dist folder wil be created

--- dayFive/dist/models.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Line = void 0;
-class Line {
-    start;
-    end;
-}
-exports.Line = Line;
-//# sourceMappingURL=models.js.map
